6e7bc359467b7d5c6d8e4a9c3e975047c1a3f10e
Miguel Pignatelli <emepyc@gmail.com>
2017-05-12 11:30:32 +0100

    reformatted + added charge force to graph

--- src/network.js
+++ src/network.js
@@ -1,348 +1,400 @@
+/* global d3:true */
+
 import apijs from 'tnt.api';
 // import {getData, highlightCluster, toggleTerminals} from './data.js';
-import {getQuery} from './data.js';
+import { getQuery } from './data';
 
 let force;
 let link;
 let node;
 let context;
 let transform = d3.zoomIdentity;
-let weightScale;
+let linkWeightScale;
+let nodeWeightScale;
 
-let config = {
-    colors: {}, // expect
-    radius: 10,
-    fields: {},
-    collapseSingletons: true,
-    query: "IL33 AND asthma",
-    data: {},
-    width: 800,
-    height: 800,
-    nodeSize: 5,
-    colorScale: d3.scaleLinear()
-        .range([d3.rgb("#007AFF"), d3.rgb('#FFF500')]) // The domain is set dynamically
+const config = {
+  colors: {}, // expect
+  fields: {},
+  // collapseSingletons: true,
+  query: 'IL33 AND asthma',
+  data: {},
+  width: 800,
+  height: 800,
+  nodeSize: 30,
+  maxNodeSize: 30,
+  minNodeSize: 5,
+  // colorScale: d3.scaleLinear()
+  //   .range([d3.rgb('#007AFF'), d3.rgb('#FFF500')]), // The domain is set dynamically
 };
 
-let dispatch = d3.dispatch("click", "dblclick", "mouseover", "mouseout", "select", "unselect", "interaction");
+const dispatch = d3.dispatch('click', 'dblclick', 'mouseover', 'mouseout', 'loaded', 'failed');
 
 export default function () {
 
-    let render = function (container) {
-        getQuery(config.query, config.fields)
-            .then (function (resp) {
-                console.log(resp);
-                config.data.nodes = resp.data.vertices;
-                config.data.links = resp.data.connections;
+  const render = function (container) {
 
-                createForce(container, config);
-            })
-            .catch(function (err) {
-                console.trace(err.message);
-            });
-    };
+    getQuery(config.query, config.fields)
+      .then((resp) => {
+        console.log(resp);
 
-    let api = apijs(render).getset(config);
+        // If there is no graph, just fire the 'failed' event
+        if (!resp.data.vertices.length || resp.status !== 200) {
+          dispatch.call('failed');
+          return;
+        }
 
-    function _toggleTerminals () {
-        config.data = toggleTerminals();
-        return this;
-    }
+        config.data.nodes = resp.data.vertices;
+        config.data.links = resp.data.connections;
 
-    api.method('toggleTerminals', _toggleTerminals);
+        createForce(container, config);
+        dispatch.call('loaded');
+      })
+      .catch((err) => {
+        console.trace(err.message);
+      });
+  };
 
-    render.update = update;
-    render.highlightCluster = _highlightCluster;
+  apijs(render).getset(config);
 
-    // return d3.rebind(render, dispatch, "on");
-    render.on = function () {
-        let value = dispatch.on.apply(dispatch, arguments);
-        return value === dispatch ? render : value;
-    };
+  // function toggleTerminals() {
+  //   config.data = toggleTerminals();
+  //   return this;
+  // }
+  //
+  // api.method('toggleTerminals', toggleTerminals);
 
-    return render;
-};
+  render.update = update;
+  render.highlightCluster = highlightCluster;
 
-function _highlightCluster(node) {
-    if (node) {
-        config.data = highlightCluster(node.id);
-    } else {
-        config.data = highlightCluster();
-    }
-    return this;
+  // return d3.rebind(render, dispatch, "on");
+  render.on = function () {
+    /* eslint prefer-spread: 0 */
+    /* eslint prefer-rest-params: 0 */
+    const value = dispatch.on.apply(dispatch, arguments);
+    return value === dispatch ? render : value;
+  };
+
+  return render;
 }
 
-function update () {
-    // let data = config.data;
-    // console.log(data);
-
-    link = link
-        .data(force.links(), (d) => `${d.source.id}-${d.target.id}`);
-    link.exit()
-        .remove();
-    link
-        .enter().append("line")
-        .attr("stroke-width", () => 1)
-        .style({"stroke": "gray"});
-
-
-    node = node
-        .data(force.nodes(), (d) => d.id);
-
-    node.exit()
-        .remove();
-    node
-        .enter()
-        .append("circle")
-        .each((d) => {
-            console.log(`new node created`);
-        })
-        .attr("cx", (d) => d.x)
-        .attr("cy", (d) => d.y)
-        .attr("r", config.nodeSize)
-        .style("stroke", (d, i) => "black")
-        .style("fill", (d) => {
-            if (d.type === 'disease') return "yellow";
-            if (d.type === 'omnipathdb') return "orange";
-            if (d.type === 'terminal') return "lightgrey";
-            return "green";
-        })
-
-        .call(force.drag)
-        .on("mousedown", () => d3.event.stopPropagation())
-        .on('click', function (d, i) {
-            if (d3.event.defaultPrevented) {
-                return;
-            }
-            dispatch.click.call(this, d, i);
-        });
-
-    node
-        .style("opacity", (d) => {
-            if (d.visible) {
-                return 1;
-            }
-            return 0.1;
-        });
-
-
-    force.start();
+function highlightCluster(n) {
+  if (n) {
+    config.data = highlightCluster(n.id);
+  }
+  else {
+    config.data = highlightCluster();
+  }
+  return this;
 }
 
-function getWeightExtent(config) {
-    let links = config.data.links;
-    let extent = d3.extent(links, function (d) {
-        return d.weight;
+function update() {
+  // let data = config.data;
+  // console.log(data);
+
+  link = link
+    .data(force.links(), (d) => `${d.source.id}-${d.target.id}`);
+  link.exit()
+    .remove();
+  link
+    .enter().append('line')
+    .attr('stroke-width', () => 1)
+    .style({ stroke: 'gray' });
+
+
+  node = node
+    .data(force.nodes(), (d) => d.id);
+
+  node.exit()
+    .remove();
+  node
+    .enter()
+    .append('circle')
+    .each(() => {
+      console.log('new node created');
+    })
+    .attr('cx', (d) => d.x)
+    .attr('cy', (d) => d.y)
+    .attr('r', config.nodeSize)
+    .style('stroke', 'black')
+    .style('fill', (d) => {
+      if (d.type === 'disease') return 'yellow';
+      if (d.type === 'omnipathdb') return 'orange';
+      if (d.type === 'terminal') return 'lightgrey';
+      return 'green';
+    })
+
+    .call(force.drag)
+    .on('mousedown', () => d3.event.stopPropagation())
+    .on('click', function (d, i) {
+      if (d3.event.defaultPrevented) {
+        return;
+      }
+      dispatch.click.call(this, d, i);
     });
-    return  d3.scaleLinear()
-        .domain(extent)
-        .range([1, config.radius*0.7])
+
+  node
+    .style('opacity', (d) => {
+      if (d.visible) {
+        return 1;
+      }
+      return 0.1;
+    });
+
+
+  force.start();
 }
 
-function createForce(container, config) {
+function getLinkWeightExtent(conf) {
+  const links = conf.data.links;
+  const extent = d3.extent(links, (d) => d.weight);
+  return d3.scaleLinear()
+    .domain(extent)
+    .range([1, conf.nodeSize * 0.7]);
+}
 
-    let meter = d3.select(container)
-        .append("div")
-        .attr("class", "interactions-network-progress")
-        .node();
+function getNodeWeightExtent(conf) {
+  const nodes = conf.data.nodes;
+  const extent = d3.extent(nodes, (d) => d.weight);
+  return d3.scaleLinear()
+    .domain(extent)
+    .range([conf.minNodeSize, conf.maxNodeSize]);
+}
 
-    weightScale = getWeightExtent(config);
+function createForce(container, conf) {
+  const meter = d3.select(container)
+    .append('div')
+    .attr('class', 'interactions-network-progress')
+    .node();
 
-    let canvas = d3.select(container)
-        .append("canvas")
-        .attr("width", config.width)
-        .attr("height", config.height);
-        // .call(d3.zoom().scaleExtent([0.5, 8]).on("zoom", zoom));
+  linkWeightScale = getLinkWeightExtent(conf);
+  nodeWeightScale = getNodeWeightExtent(conf);
 
-    context = canvas.node().getContext("2d");
+  const canvas = d3.select(container)
+    .append('canvas')
+    .attr('width', conf.width)
+    .attr('height', conf.height);
+  // .call(d3.zoom().scaleExtent([0.5, 8]).on("zoom", zoom));
 
-    // let color = d3.scaleOrdinal(d3.schemeCategory20);
-    // let color = "#aa5566";
+  context = canvas.node().getContext('2d');
 
-    let data = config.data;
+  // let color = d3.scaleOrdinal(d3.schemeCategory20);
+  // let color = "#aa5566";
 
-    // Initialise the all the nodes in a sensible place
-    data.nodes[0].x = ~~config.width / 3;
-    data.nodes[0].y = config.height / 2;
-    data.nodes[1].x = ~~(config.width - config.width / 3);
-    data.nodes[1].y = ~~config.height / 2;
-    for (let k = 2; k < data.nodes.length; k++) {
-        data.nodes[k].x = ~~config.width / 2;
-        data.nodes[k].y = ~~config.height / 2;
+  const data = conf.data;
+
+  // Initialise the all the nodes in a sensible place
+  data.nodes[0].x = ~~conf.width / 3;
+  data.nodes[0].y = conf.height / 2;
+  data.nodes[1].x = ~~(conf.width - (conf.width / 3));
+  data.nodes[1].y = ~~conf.height / 2;
+  for (let k = 2; k < data.nodes.length; k += 1) {
+    data.nodes[k].x = ~~conf.width / 2;
+    data.nodes[k].y = ~~conf.height / 2;
+  }
+
+
+  force = d3.forceSimulation(data.nodes)
+    //.force('links', d3.forceLink(data.links).strength(1).distance(300).iterations(1))
+    .force('charge', d3.forceManyBody().strength(-10))
+    .force('center', d3.forceCenter().x(conf.width / 2).y(conf.height / 2))
+    .force('collision', d3.forceCollide().radius(conf.nodeSize * 3))
+    .force('links', d3.forceLink(data.links))
+    .stop();
+
+  // .on("tick", ticked)
+  // .on("end", ended);
+
+  canvas
+    .call(d3.drag()
+      .container(canvas.node())
+      .subject(dragsubject)
+      .on('drag', dragged),
+    )
+    .call(d3.zoom()
+      .scaleExtent([0.01, 5])
+      .on('zoom', zoomed),
+    );
+
+  // Ticks
+  const n = Math.ceil(Math.log(force.alphaMin()) / Math.log(1 - force.alphaDecay()));
+  for (let i = 1; i <= n; i += 1) {
+    force.tick();
+  }
+
+  // And clear the progressing bar
+  ticked();
+  ended();
+
+
+  function ticked() {
+    const progress = 1 - force.alpha();
+    meter.style.width = `${100 * progress}%`;
+    redraw();
+  }
+
+  function redraw() {
+    context.clearRect(0, 0, conf.width, conf.height);
+    context.save();
+
+    context.translate(transform.x, transform.y);
+    context.scale(transform.k, transform.k);
+
+    // Draw links
+    data.links.forEach(drawLink);
+
+    // Draw nodes
+    data.nodes.forEach(drawNode);
+
+    context.restore();
+  }
+
+  function ended() {
+    meter.style.display = 'none';
+  }
+
+  function dragsubject() {
+    /* eslint consistent-return: 0 */
+    const x = transform.invertX(d3.event.x);
+    const y = transform.invertY(d3.event.y);
+    console.log(`${x},${y}`);
+    const thisNode = force.find(x, y, conf.maxNodeSize);
+    console.log(thisNode);
+    if (!thisNode) return;
+    thisNode.x = transform.applyX(thisNode.x);
+    thisNode.y = transform.applyY(thisNode.y);
+    console.log('returning this node...');
+    console.log(thisNode);
+    console.log(d3.event);
+    return thisNode;
+  }
+
+
+  // function dragstarted() {
+  //   // if (!d3.event.active) force.alphaTarget(0.3).restart();
+  //   d3.event.subject.fx = d3.event.subject.x;
+  //   d3.event.subject.fy = d3.event.subject.y;
+  // }
+
+  function dragged() {
+    // d3.event.subject.fx = d3.event.x;
+    // d3.event.subject.fy = d3.event.y;
+    d3.event.subject.x = transform.invertX(d3.event.x);
+    d3.event.subject.y = transform.invertY(d3.event.y);
+    redraw();
+  }
+
+  function clicked() {
+    d3.event.defaultPrevented;
+    const clickedNode = dragsubject();
+    console.log('clicked node is...');
+    console.log(clickedNode);
+  }
+
+  // function dragended() {
+  //   if (!d3.event.active) force.alphaTarget(0);
+  //   d3.event.subject.fx = null;
+  //   d3.event.subject.fy = null;
+  // }
+
+  function drawLink(d) {
+    context.beginPath();
+    context.strokeStyle = '#8da0cb';
+    context.globalAlpha = 0.5;
+    context.moveTo(d.source.x, d.source.y);
+    context.lineWidth = linkWeightScale(d.weight);
+    context.lineTo(d.target.x, d.target.y);
+    context.stroke();
+    context.globalAlpha = 1;
+  }
+
+  function drawNode(d) {
+    const nodeSize = nodeWeightScale(d.weight);
+
+    // Node
+    context.beginPath();
+    context.moveTo(d.x, d.y);
+    context.fillStyle = conf.colors[d.field];
+    context.globalAlpha = 0.5;
+    context.arc(d.x, d.y, nodeSize, 0, 2 * Math.PI);
+    context.fill();
+
+    // circle
+    context.beginPath();
+    context.globalAlpha = 1;
+    context.arc(d.x, d.y, nodeSize + 1, 0, 2 * Math.PI);
+    context.strokeStyle = conf.colors[d.field];
+    context.stroke();
+    if (d.selected) {
+      context.beginPath();
+      context.globalAlpha = 1;
+      context.arc(d.x, d.y, nodeSize + 5, 0, 2 * Math.PI);
+      context.strokeStyle = conf.colors[d.field];
+      context.stroke();
     }
 
+    // label
+    context.textAlign = 'center';
+    context.fillStyle = 'black';
+    context.fillText(d.term, d.x, d.y + (nodeSize + 15));
+  }
 
-    force = d3.forceSimulation(data.nodes)
-        // .force("charge", d3.forceManyBody().strength(-30))
-        .force("center", d3.forceCenter().x(config.width/2).y(config.height/2))
-        // .force("links", d3.forceLink(data.links).strength(1).distance(150).iterations(10))
-        .force("collision", d3.forceCollide().radius(30))
-        .force("links", d3.forceLink(data.links))
-        .stop();
+  function zoomed() {
+    transform = d3.event.transform;
+    redraw();
+  }
 
-        // .on("tick", ticked)
-        // .on("end", ended);
-
-    canvas
-        .call(d3.drag()
-            .container(canvas.node())
-            .subject(dragsubject)
-            .on("drag", dragged)
-        )
-        .call(d3.zoom()
-            .scaleExtent([0.5, 5])
-            .on("zoom", zoomed));
-
-
-    // Ticks
-    let n = Math.ceil(Math.log(force.alphaMin()) / Math.log(1 - force.alphaDecay()));
-    for (let i=0; i<n; ++i) {
-        force.tick();
-    }
-
-    // And clear the progressing bar
-    ticked();
-    ended();
-
-
-    function ticked() {
-        let progress = 1-force.alpha();
-        meter.style.width = 100 * progress + "%";
-        redraw();
-    }
-
-    function redraw() {
-        context.clearRect(0, 0, config.width, config.height);
-        context.save();
-
-        context.translate(transform.x, transform.y);
-        context.scale(transform.k, transform.k);
-
-        // Draw links
-        context.strokeStyle = "#aaa";
-        data.links.forEach(drawLink);
-
-        // Draw nodes
-        data.nodes.forEach(drawNode);
-
-        context.restore();
-    }
-
-    function ended() {
-        meter.style.display = "none";
-    }
-
-    function dragsubject() {
-        let x = transform.invertX(d3.event.x);
-        let y = transform.invertY(d3.event.y);
-
-        let thisNode = force.find(x, y, config.radius);
-        if (!thisNode) return;
-        thisNode.x = transform.applyX(thisNode.x);
-        thisNode.y = transform.applyY(thisNode.y);
-        return thisNode;
-    }
-
-
-    function dragstarted() {
-        // if (!d3.event.active) force.alphaTarget(0.3).restart();
-        d3.event.subject.fx = d3.event.subject.x;
-        d3.event.subject.fy = d3.event.subject.y;
-    }
-
-    function dragged() {
-        // d3.event.subject.fx = d3.event.x;
-        // d3.event.subject.fy = d3.event.y;
-        d3.event.subject.x = transform.invertX(d3.event.x);
-        d3.event.subject.y = transform.invertY(d3.event.y);
-        redraw();
-    }
-
-    function dragended() {
-        if (!d3.event.active) force.alphaTarget(0);
-        d3.event.subject.fx = null;
-        d3.event.subject.fy = null;
-    }
-
-    function drawLink(d) {
-        context.beginPath();
-        context.moveTo(d.source.x, d.source.y);
-        context.lineWidth=weightScale(d.weight);
-        context.lineTo(d.target.x, d.target.y);
-        context.stroke();
-
-    }
-
-    function drawNode(d) {
-        context.beginPath();
-        context.moveTo(d.x, d.y);
-        context.fillStyle = config.colors[d.field];
-        context.arc(d.x, d.y, config.radius, 0, 2 * Math.PI);
-        context.fill();
-        context.textAlign="center";
-        context.fillText(d.term, d.x, d.y+20);
-        context.strokeStyle = "#aaa";
-        context.stroke();
-
-    }
-
-    function zoomed() {
-        transform = d3.event.transform;
-        redraw();
-    }
-
-    // function tick(e) {
-    //     console.log(`alpha: ${force.alpha()}`);
-    //
-    //     link
-    //         .attr("x1", (d) => d.source.x)
-    //         .attr("y1", (d) => d.source.y)
-    //         .attr("x2", (d) => d.target.x)
-    //         .attr("y2", (d) => d.target.y);
-    //
-    //     node
-    //         .attr("cx", (d) => d.x)
-    //         .attr("cy", (d) => d.y);
-    // }
-    //
-    //
-    // link = svg.append("g")
-    //     .attr("class", "links")
-    //     .selectAll("line");
-        // .selectAll("line");
-    //     .data(data.links, (d) => `${d.source.id}-${d.target.id}`);
-    // link.exit("remove");
-    // link
-    //     .enter().append("line")
-    //     .attr("stroke-width", () => 1)
-    //     .style({"stroke": "gray"});
-    //
-    //
-    // node = svg.append("g")
-    //     .attr("class", "nodes")
-    //     .selectAll("circle");
-        // .selectAll("circle");
-    //     .data(data.nodes, (d) => d.id);
-    // node.exit("remove");
-    // node
-    //     .enter().append("circle")
-    //     .attr("cx", (d) => d.x)
-    //     .attr("cy", (d) => d.y)
-    //     .attr("r", config.nodeSize)
-    //     .style("opacity", (d) => {
-    //         if (d.visible) {
-    //             return 1;
-    //         }
-    //         return 0.1;
-    //     })
-    //     .style("fill", (d) => {
-    //         if (d.type === 'disease') return "yellow";
-    //         if (d.type === 'omnipathdb') return "orange";
-    //         if (d.type === 'terminal') return "lightgrey";
-    //         return "green";
-    //     })
-    //     .style("stroke", (d, i) => "black")
-    //     .call(force.drag)
-    //     .on("mousedown", () => d3.event.stopPropagation())
-    //     .on('click', (d) => console.log(d));
-
+  // function tick(e) {
+  //     console.log(`alpha: ${force.alpha()}`);
+  //
+  //     link
+  //         .attr("x1", (d) => d.source.x)
+  //         .attr("y1", (d) => d.source.y)
+  //         .attr("x2", (d) => d.target.x)
+  //         .attr("y2", (d) => d.target.y);
+  //
+  //     node
+  //         .attr("cx", (d) => d.x)
+  //         .attr("cy", (d) => d.y);
+  // }
+  //
+  //
+  // link = svg.append("g")
+  //     .attr("class", "links")
+  //     .selectAll("line");
+  // .selectAll("line");
+  //     .data(data.links, (d) => `${d.source.id}-${d.target.id}`);
+  // link.exit("remove");
+  // link
+  //     .enter().append("line")
+  //     .attr("stroke-width", () => 1)
+  //     .style({"stroke": "gray"});
+  //
+  //
+  // node = svg.append("g")
+  //     .attr("class", "nodes")
+  //     .selectAll("circle");
+  // .selectAll("circle");
+  //     .data(data.nodes, (d) => d.id);
+  // node.exit("remove");
+  // node
+  //     .enter().append("circle")
+  //     .attr("cx", (d) => d.x)
+  //     .attr("cy", (d) => d.y)
+  //     .attr("r", config.nodeSize)
+  //     .style("opacity", (d) => {
+  //         if (d.visible) {
+  //             return 1;
+  //         }
+  //         return 0.1;
+  //     })
+  //     .style("fill", (d) => {
+  //         if (d.type === 'disease') return "yellow";
+  //         if (d.type === 'omnipathdb') return "orange";
+  //         if (d.type === 'terminal') return "lightgrey";
+  //         return "green";
+  //     })
+  //     .style("stroke", (d, i) => "black")
+  //     .call(force.drag)
+  //     .on("mousedown", () => d3.event.stopPropagation())
+  //     .on('click', (d) => console.log(d));
 }
